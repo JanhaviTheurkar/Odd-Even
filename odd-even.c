65083a43a460378f1c268e2074e9d570daccc1ab
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-09-10 16:28:32 +0530

    Add files via upload

---
+++ odd-even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+    int num;
+    printf("Enter Number ",num);
+    scanf("%d",&num);
+
+    if (num %2==0)
+    {
+        printf("even");
+    }
+    else{
+        printf("odd");
+    }
+    
+}
